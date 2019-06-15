3f2ba11ab1eb2e4c778ffa9975c8a133edbd84a9
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2019-06-15 12:28:02 -0400

    added toolbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { MaterialModule } from '../shared/material.module';
+import { ToolbarComponent } from '../ui/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ToolbarComponent],
   imports: [
     CommonModule,
     SharedModule,
-    MaterialModule
-  ]
+    MaterialModule,
+  ],
+  exports: [ToolbarComponent]
 })
 export class CoreModule { }
